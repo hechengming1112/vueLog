99dfbee28d6a01bff65f8828efa3d848370a0a4f
hechengming1112 <15038313604@163.com>
2019-12-20 15:05:41 +0800

    第一次提交

---
+++ myVue/src/components/PostArticle.vue
@@ -0,0 +1,69 @@
+<template>
+    
+</template>
+<script>
+export default {
+    data(){
+        return {
+            categories: [],
+            tagInputVisible: false,
+            tagValue: '',
+            loading: false,
+            from: '',
+            article: {
+            id: '-1',
+            dynamicTags: [],
+            title: '',
+            mdContent: '',
+            cid: ''
+        }
+    }
+    
+}
+}
+</script>
+<style >
+ .post-article > .main > #editor {
+    width: 100%;
+    height: 450px;
+    text-align: left;
+  }
+
+  .post-article > .header {
+    background-color: #ececec;
+    margin-top: 10px;
+    padding-left: 5px;
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  .post-article > .main {
+    /*justify-content: flex-start;*/
+    display: flex;
+    flex-direction: column;
+    padding-left: 5px;
+    background-color: #ececec;
+    padding-top: 0px;
+  }
+
+  .post-article > .header > .el-tag + .el-tag {
+    margin-left: 10px;
+  }
+
+  .post-article > .header > .button-new-tag {
+    margin-left: 10px;
+    height: 32px;
+    line-height: 30px;
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+
+  .post-article > .header > .input-new-tag {
+    width: 90px;
+    margin-left: 10px;
+    vertical-align: bottom;
+  }
+
+  .post-article {
+  }
+</style>
